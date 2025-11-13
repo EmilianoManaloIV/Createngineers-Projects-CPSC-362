3bfad143d4a76637430c6ea6f934ea193218a5c9
ColinMercado <colinmercado24@gmail.com>
2025-11-13 00:15:00 -0800

    Added a Card draw Animation + Added more Game State Scenes
    Added sample scenes for when you're making and then playing the game; added card draw animation for when you draw a card while playing.

--- Assets/Scripts/StartScreenScripts/StartMenuController.cs
+++ Assets/Scripts/StartScreenScripts/StartMenuController.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartMenuController : MonoBehaviour
 {
     public void OnStartClick()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("MainGameScene");
+        // Change "StartGameScene" to the actual name of the game scene
+        SceneManager.LoadScene("StartGameScene");
     }
     public void OnExitClick()
     {
